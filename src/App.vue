227cf2f142268ae28ca93a509be998823c54974a
dae <cys964@gmail.com>
2024-10-08 20:48:08 +0900

    헤더 수정 및 store추가

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,26 @@
 <template>
     <div class="app-container" :class="{ 'home-background': isHomePage }">
-        <header-compo v-if="showHeader" :class="{ 'header-scrolled': isHeaderScrolled }"></header-compo>
-        <page-main v-else-if="isHomePage && !isLoggedIn"></page-main>
-        <middle-compo v-if="showMiddleCompo"></middle-compo>
-        <fairy-player v-else></fairy-player>
-        <footer-compo v-if="showHeader"></footer-compo>
+        <header-compo
+            v-if="showHeader"
+            :class="{ 'header-scrolled': isHeaderScrolled }"
+            :show-nav="showNav"
+        ></header-compo>
+        <page-main v-if="isHomePage && !isLoggedIn"></page-main>
+        <template v-else>
+            <middle-compo v-if="showMiddleCompo"></middle-compo>
+            <fairy-player v-else></fairy-player>
+        </template>
     </div>
 </template>
 
 <script setup>
-import { computed, onMounted, ref, onUnmounted } from 'vue';
+import { computed, onMounted, ref, onUnmounted, watch } from 'vue';
 import { useRoute } from 'vue-router';
 import { storeToRefs } from 'pinia';
 import { useProfileStore } from './stores/profile';
+import { useLayoutStore } from './stores/layout';
 
-import FooterCompo from './components/layout/FooterCompo.vue';
+// import FooterCompo from './components/layout/FooterCompo.vue';
 import HeaderCompo from './components/layout/HeaderCompo.vue';
 import MiddleCompo from './components/layout/MiddleCompo.vue';
 import FairyPlayer from './components/pages/FairyPlayer.vue';
@@ -22,6 +28,7 @@ import PageMain from './components/pages/PageMain.vue';
 
 const route = useRoute();
 const profileStore = useProfileStore();
+const layoutStore = useLayoutStore();
 const { isLoggedIn } = storeToRefs(profileStore);
 
 const isHeaderScrolled = ref(false);
@@ -41,12 +48,6 @@ onUnmounted(() => {
 
 const showHeader = computed(() => {
     const hiddenHeaderRoutes = ['/profiles', /^\/fairyplayer\/\d+$/];
-    const isMainPage = route.path === '/';
-
-    if (isMainPage && !isLoggedIn.value) {
-        return false;
-    }
-
     return !hiddenHeaderRoutes.some((hiddenRoute) =>
         typeof hiddenRoute === 'string'
             ? hiddenRoute === route.path.toLowerCase()
@@ -58,7 +59,18 @@ const showMiddleCompo = computed(() => {
     return !/^\/fairyplayer\/\d+$/.test(route.path.toLowerCase());
 });
 
-// onNaverLogin 함수 제거
+const isHomePage = computed(() => route.path === '/');
+
+const showNav = computed(() => {
+    return route.name === 'FairyTaleList';
+});
+
+watch(
+    () => route.name,
+    (newRouteName) => {
+        layoutStore.setShowNav(newRouteName === 'FairyTaleList');
+    },
+);
 </script>
 
 <style>
