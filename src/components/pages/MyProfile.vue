2e75a77422187f80cb842c2645b95ba986c06d90
Chani17 <cucute8878@naver.com>
2024-10-09 13:43:07 +0900

    Update: My profile css code

--- src/components/pages/MyProfile.vue
+++ src/components/pages/MyProfile.vue
@@ -3,7 +3,6 @@
         <img :src="user.profileImage" alt="프로필 이미지" class="profile-img" />
         <h2>{{ user.name }}</h2>
         <p>{{ user.gender }}</p>
-        <p>{{ user.birthdate }}</p>
     </div>
 
     <div class="stats">
@@ -12,7 +11,7 @@
             <p>{{ user.totalPurchases }}</p>
         </div>
         <div>
-            <p>대여 횟수</p>
+            <p>총 대여 횟수</p>
             <p>{{ user.totalRentals }}</p>
         </div>
     </div>
@@ -35,20 +34,42 @@ const user = {
 }
 
 .profile-img {
-    width: 150px;
-    height: 150px;
-    border-radius: 50%;
+    width: 200px;
+    height: 200px;
+}
+
+.profile-info h2 {
+    font-size: 36px;
+}
+
+.profile-info p {
+    font-size: 16px;
+    margin: 10px 0;
 }
 
 .stats {
     display: flex;
     justify-content: center;
-    margin-top: 20px;
+    margin-top: 50px;
 }
 
 .stats div {
+    width: 150px;
+    height: 100%;
     padding: 10px 20px;
     border: 1px solid #ddd;
     margin: 0 10px;
+    border-radius: 10%;
+    text-align: center;
+}
+
+.stats p:first-child { /* 총 구매권수, 총 대여 횟수 */
+    font-size: 26px;
+}
+
+.stats p:last-child { /* user.totalPurchases와 user.totalRentals */
+    font-weight: bold;
+    font-size: 26px;
+    color:darkblue;
 }
 </style>
