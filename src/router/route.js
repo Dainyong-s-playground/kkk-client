9c0843a21970e77ccae7a4ea14aa76d4c01fc28b
Chani17 <cucute8878@naver.com>
2024-10-09 13:11:10 +0900

    Add: Mypage path

--- src/router/route.js
+++ src/router/route.js
@@ -2,6 +2,7 @@ import MiddleFront from '@/components/layout/MiddleFront.vue';
 import FairyPlayer from '@/components/pages/FairyPlayer.vue';
 import FairyTaleList from '@/components/pages/FairyTaleList.vue';
 import HandLandmark from '@/components/pages/Game/HandLandmark.vue';
+import MyPage from '@/components/pages/MyPage.vue';
 import PageMain from '@/components/pages/PageMain.vue';
 import ProfileView from '@/components/pages/ProfileView.vue';
 
@@ -33,6 +34,10 @@ const routes = [
     {
         path: '/game/hand',
         component: HandLandmark,
+    },
+    {
+        path: '/mypage',
+        component: MyPage,
     },
 ];
 
