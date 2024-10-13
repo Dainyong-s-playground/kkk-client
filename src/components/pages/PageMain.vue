58609bcae4c2e7223b436e7fd8f3530426e9fcd8
dae <cys964@gmail.com>
2024-10-14 05:35:55 +0900

    메인 페이지 배경 투명도 조절

--- src/components/pages/PageMain.vue
+++ src/components/pages/PageMain.vue
@@ -114,7 +114,7 @@ const mainPageStyle = computed(() => ({
     width: 100%;
     height: 100%;
     z-index: 0;
-    background-color: rgba(255, 255, 255, 0.2);
+    background-color: rgba(0, 0, 0, 0.5);
 }
 
 .content-wrapper {
